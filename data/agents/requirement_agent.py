22f18f64c63e68641b7b3d19ca98f96c6632e043
SUBHASRI-785 <subhasris587@gmail.com>
2026-08-30 21:26:05 +0530

    Add analyze_requirement function for user queries

---
+++ data/agents/requirement_agent.py
@@ -0,0 +1,11 @@
+def analyze_requirement(user_query):
+    """
+    Analyze the user's shopping requirement.
+    """
+
+    requirement = {
+        "query": user_query,
+        "status": "Requirement received"
+    }
+
+    return requirement
